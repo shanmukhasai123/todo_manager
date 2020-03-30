b333a13d4281f1ad66a496731a3b410322024b9c
shanmukhasai123 <shanmukhasaicheedella@gmail.com>
2020-03-30 14:30:32 +0530

    after creating table using migration

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 class TodosController<ApplicationController
     skip_before_action :verify_authenticity_token
-    
+
     def index
         render plain: Todo.all.map{|todo| todo.to_displayable}.join("\n")
         
@@ -15,5 +15,14 @@ class TodosController<ApplicationController
         due_date=DateTime.parse(params[:due_date])
         new_todo=Todo.create!(todo_text:todo_text,due_date:due_date,completed:false)
         render plain: "created new_todo with id:#{new_todo.id}"
+    end
+    def update
+        id=params[:id]
+        completed=params[:completed]
+        todo=Todo.find(id)
+        todo.completed=completed
+        todo.save!
+        render plain: "todo is updated with value #{completed}"
+
     end
 end
