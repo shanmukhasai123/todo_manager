52609f84dcb64233eb8f58d639fec491e20fd6e2
shanmukhasai123 <shanmukhasaicheedella@gmail.com>
2020-04-07 00:35:22 +0530

    after creating migration for users

--- db/migrate/20200330082835_users.rb
+++ db/migrate/20200330082835_users.rb
@@ -1,10 +1,13 @@
 class Users < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-      t.timestamps
-    end
+  def self.up
+    #create_table :users do |t|
+     # t.string :name
+      #t.string :email
+      #t.string :password
+      #t.timestamps
+    #end
+    User.create :name =>"kalyani", :email=>"cheedellakalyani007@gmail.com", :password=>"kalyani"
+    User.create :name =>"gupta", :email=>"cheedellagupta.kumari@gmail.com", :password=>"gupta9000"
+    User.create :name =>"shanmukhasai1", :email=>"cheedellashanmukha@gmail.com", :password=>"shanmukha9000"
   end
 end
