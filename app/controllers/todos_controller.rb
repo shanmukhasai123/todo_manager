fa1036e18d89f7b7275a5c7a295b976843507bfe
shanmukhasai123 <shanmukhasaicheedella@gmail.com>
2020-05-14 01:47:56 +0530

    deleting the todo using trash icon

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -27,4 +27,11 @@ class TodosController < ApplicationController
     todo.save!
     redirect_to todos_path
   end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.destroy
+    redirect_to todos_path
+  end
 end
