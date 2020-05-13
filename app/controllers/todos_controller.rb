ac7fe6e4595e18a57208a033cf38a30dd26f8838
shanmukhasai123 <shanmukhasaicheedella@gmail.com>
2020-05-13 19:09:00 +0530

    after adding form element to add a new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,28 +1,30 @@
-class TodosController<ApplicationController
-    skip_before_action :verify_authenticity_token
+class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
-    def index
-        #render plain: Todo.all.map{|todo| todo.to_displayable}.join("\n")
-        render "index"
-    end
-    def show
-        id=params[:id]
-        todo=Todo.find(id)
-        render plain: todo.to_displayable
-    end
-    def create
-        todo_text=params[:todo_text]
-        due_date=DateTime.parse(params[:due_date])
-        new_todo=Todo.create!(todo_text:todo_text,due_date:due_date,completed:false)
-        render plain: "created new_todo with id:#{new_todo.id}"
-    end
-    def update
-        id=params[:id]
-        completed=params[:completed]
-        todo=Todo.find(id)
-        todo.completed=completed
-        todo.save!
-        render plain: "todo is updated with value #{completed}"
+  def index
+    #render plain: Todo.all.map{|todo| todo.to_displayable}.join("\n")
+    render "index"
+  end
 
-    end
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render plain: todo.to_displayable
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    redirect_to todos_path
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "todo is updated with value #{completed}"
+  end
 end
